1640c34ce5efc2efefa354bf5dbbb89afd60c46b
nargil07 <antony.marchi@lyceeastier.com>
2015-12-03 10:54:36 +0100

    Revert "Intent mesures"
    This reverts commit a959ca8550c7304052e153f59893cd05d8a502f3.

--- app/src/main/java/com/example/antony/tp_meteo/ListStationActivity.java
+++ app/src/main/java/com/example/antony/tp_meteo/ListStationActivity.java
@@ -1,14 +1,10 @@
 package com.example.antony.tp_meteo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.antony.tp_meteo.list_adapter.StationAdapter;
-import com.example.antony.tp_meteo.metier.Mesure;
 import com.example.antony.tp_meteo.metier.Station;
 
 import java.util.ArrayList;
@@ -17,32 +13,15 @@ public class ListStationActivity extends AppCompatActivity {
 
     ListView listView_station;
     ArrayList<Station> list_station = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_station);
 
-        ArrayList<Mesure> mesureArrayList = new ArrayList<>();
-        mesureArrayList.add(new Mesure("2015-10-25 10:00:00", "5.5", "4", "1008", null, null, null, null));
-
-        list_station.add(new Station("Montélimar", "Les station du sud", mesureArrayList));
-        mesureArrayList = new ArrayList<>();
-        mesureArrayList.add(new Mesure("2015-10-25 10:00:00", "5.5", "4", "1008", null, null, null, null));
-        mesureArrayList.add(new Mesure("2015-10-25 10:20:00", "15.5", "6", "1008", null, null, null, null));
-        mesureArrayList.add(new Mesure("2015-10-25 10:00:00", "25.5", "9", "1008", null, null, null, null));
-        list_station.add(new Station("Albousiere", "Les station du nord", mesureArrayList));
+        list_station.add(new Station("Montélimar", "Les station du sud"));
+        list_station.add(new Station("Albousiere", "Les station du nord"));
 
         listView_station = (ListView) findViewById(R.id.listView_station);
         listView_station.setAdapter(new StationAdapter(list_station, this));
-        listView_station.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                StationAdapter stationAdapter = (StationAdapter) parent.getAdapter();
-                Intent i = new Intent(getApplicationContext(), ListMesureActivity.class);
-                i.putExtra("station", stationAdapter.getItem(position));
-                startActivity(i);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/antony/tp_meteo/metier/Station.java
+++ app/src/main/java/com/example/antony/tp_meteo/metier/Station.java
@@ -1,24 +1,18 @@
 package com.example.antony.tp_meteo.metier;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by antony on 19/11/2015.
  */
-public class Station implements Serializable{
+public class Station {
     String identifiant;
     String libelle;
-    ArrayList<Mesure> mesureList;
 
     public Station() {
     }
 
-    public Station(String libelle, String identifiant, ArrayList<Mesure> mesureList) {
+    public Station(String libelle, String identifiant) {
         this.libelle = libelle;
         this.identifiant = identifiant;
-        this.mesureList = mesureList;
     }
 
     public String getIdentifiant() {
@@ -35,13 +29,5 @@ public class Station implements Serializable{
 
     public void setLibelle(String libelle) {
         this.libelle = libelle;
-    }
-
-    public ArrayList<Mesure> getMesureList() {
-        return mesureList;
-    }
-
-    public void setMesureList(ArrayList<Mesure> mesureList) {
-        this.mesureList = mesureList;
     }
 }
